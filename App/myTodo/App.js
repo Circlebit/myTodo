ff07b53055de5dd279a140e41bac7046c8875c33
Circlebit <paul.seibel@mailbox.org>
2019-05-13 09:01:06 +0200

    screen component ItemsList anlegen

--- App/myTodo/App.js
+++ App/myTodo/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import ItemsList from './screens/ItemsList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <ItemsList />
     );
   }
 }
---
+++ App/myTodo/screens/ItemsList.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Text, SafeAreaView } from "react-native";
+import GlobalStyles from "../config/GlobalStyles"
+
+class ItemsList extends Component {
+    state = {
+        allItems: []
+    }
+
+    render() {
+        return (
+            <SafeAreaView style={GlobalStyles.droidSafeArea}>
+                <Text>Hallo Welt!</Text>
+            </SafeAreaView>
+        );
+    }
+}
+
+export default ItemsList;
