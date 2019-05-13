78fb3d000b02bf603e1bbcdcc9a3bdc3a1a16d75
Circlebit <paul.seibel@mailbox.org>
2019-05-13 13:16:47 +0200

    Action Button auf ImtesList hinzugefügt

--- App/myTodo/screens/ItemsList.js
+++ App/myTodo/screens/ItemsList.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 import { Text, SafeAreaView } from "react-native";
-import GlobalStyles from "../config/GlobalStyles"
+import GlobalStyles from "../config/GlobalStyles";
+import ActionButton from 'react-native-action-button';
 
 class ItemsList extends Component {
     state = {
         allItems: []
     }
 
+    handleAddEvent = () => {
+        this.props.navigation.navigate('list');
+    }
+
     render() {
         return [
             <SafeAreaView style={GlobalStyles.droidSafeArea}>
                 <Text>Hallo Welt!!!</Text>
-            </SafeAreaView>
+            </SafeAreaView>,
+            <ActionButton 
+                key="fab"
+                onPress={this.handleAddEvent}
+                buttonColor="rgba(231, 76, 60, 1)"
+            />
         ];
     }
 }
