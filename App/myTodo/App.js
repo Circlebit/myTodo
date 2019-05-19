327c0e341d7c4187d260d3b890bd72576359fa2a
Circlebit <paul.seibel@mailbox.org>
2019-05-19 23:13:36 +0200

    custom drawer component

--- App/myTodo/App.js
+++ App/myTodo/App.js
@@ -1,30 +1,48 @@
 import React, { Component } from "react";
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, ScrollView } from 'react-native';
 import ItemsList from './screens/ItemsList';
 import NewItem from './screens/NewItem';
 import ProjectsList from './screens/ProjectsList'
-import { createDrawerNavigator, createAppContainer } from 'react-navigation';
+import { createDrawerNavigator, createAppContainer, DrawerItems } from 'react-navigation';
+import GlobalStyles from "./config/GlobalStyles";
 
-const AppNavigator = createDrawerNavigator({
-  list: { 
-    screen: ItemsList,
-    navigationOptions: () => ({
-      drawerLabel: 'Meine Aufgaben',
-    }),
-   },
-  newItem: { 
-    screen: NewItem,
-    navigationOptions: () => ({
-      drawerLabel: 'Neue Aufgabe',
-    }),
+const CustomDrawerContentComponent = props => (
+  <ScrollView>
+    <SafeAreaView style={GlobalStyles.droidSafeArea} forceInset={{ top: 'always', horizontal: 'never' }}>
+      <DrawerItems {...props} />
+    </SafeAreaView>
+  </ScrollView>
+);
+
+const NavigatorConfig = {
+  contentComponent: CustomDrawerContentComponent,
+  drawerBackgroundColor: 'white',
+  drawerWidth: 200,
+}
+
+const AppNavigator = createDrawerNavigator(
+  {
+    list: {
+      screen: ItemsList,
+      navigationOptions: () => ({
+        title: 'Meine Aufgaben'
+      }),
+    },
+    // newItem: { 
+    //   screen: NewItem,
+    //   navigationOptions: () => ({
+    //     drawerLabel: 'Neue Aufgabe',
+    //   }),
+    // },
+    projects: {
+      screen: ProjectsList,
+      navigationOptions: () => ({
+        drawerLabel: 'Projekte',
+      }),
+    },
   },
-  projects: { 
-    screen: ProjectsList,
-    navigationOptions: () => ({
-      drawerLabel: 'Projekte',
-    }),
-  },
-});
+  NavigatorConfig
+);
 
 const AppContainer = createAppContainer(AppNavigator);
 
